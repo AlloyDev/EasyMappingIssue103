82b85bdd296ebf576725fb138318589aea7939f7
Tsyganov Stanislav <s.tsyganov@mac139rds.local>
2015-06-30 23:06:04 +0300

    Initial Commit

---
+++ EasyMappingIssue/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EasyMappingIssue
+//
+//  Created by Tsyganov Stanislav on 30/06/15.
+//  Copyright (c) 2015 DevAlloy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EasyMappingIssue/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EasyMappingIssue
+//
+//  Created by Tsyganov Stanislav on 30/06/15.
+//  Copyright (c) 2015 DevAlloy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
